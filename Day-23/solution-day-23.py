f0d2d9b61d1c218f34c3f8653d7b45f6005fe3c0
M786453 <ahteshamsarwar333@gmail.com>
2024-12-25 21:41:59 +0500

    Add solution for puzzle#2 of day 23.

--- Day-23/solution-day-23.py
+++ Day-23/solution-day-23.py
@@ -19,31 +19,72 @@ for net in networks:
     add_computer_conn(c1, c2)
     add_computer_conn(c2, c1)
 
-# Pairs of Three Interconnected computers set
-three_ics_pair = set()
+def solve_puzzle_1():
+    # Pairs of Three Interconnected computers set
+    three_ics_pair = set()
 
-for c in computers_connections:
+    for c in computers_connections:
 
-    connected_computers = computers_connections[c] # computers connected to current computer
+        connected_computers = computers_connections[c] # computers connected to current computer
 
-    for i in range(len(connected_computers)):
+        for i in range(len(connected_computers)):
 
-        for j in range(len(connected_computers)):
+            for j in range(len(connected_computers)):
+
+                c1 = connected_computers[i]
+                c2 = connected_computers[j]
+
+                if c1 != c2:
+                    
+                    if c1 in computers_connections[c2]:
+                        ics = [c, c1, c2] # Interconnected computers
+                        if any([c.startswith('t')for c in ics]):
+                            ics.sort()
+                            three_ics_pair.add(tuple(ics))
+
+    print('Answer Puzzle#1:',len(three_ics_pair))
+
+def solve_puzzle_2():
+
+    largest_set = []
+
+    for c in computers_connections:
+
+        connected_computers = computers_connections[c] # computers connected to current computer
+
+        ics = [] # interconnected computers
+
+        ics.append(c)
+
+        for i in range(len(connected_computers)):
 
             c1 = connected_computers[i]
-            c2 = connected_computers[j]
 
-            if c1 != c2:
+            connections_list = [c1]
+
+            for j in range(len(connected_computers)):
+
+                c2 = connected_computers[j]
+
+                if c1 != c2:
+                    
+                    if c1 in computers_connections[c2]:
+                        connections_list.append(c2)
+            
+            if len(connections_list) == len(connected_computers)-1: # Connected to all
+
+                ics.append(c1)           
+
+        if len(ics) > len(largest_set):
+
+            largest_set = ics
+    
+    largest_set.sort()
+
+    print("Answer Puzzle#2:", ",".join(largest_set))
+
+solve_puzzle_1()
+solve_puzzle_2()
                 
-                if c1 in computers_connections[c2]:
-                    ics = [c, c1, c2] # Interconnected computers
-                    if any([c.startswith('t')for c in ics]):
-                        ics.sort()
-                        three_ics_pair.add(tuple(ics))
-
-print(len(three_ics_pair))
-# for ics in three_ics_pair:
-#     ics = list(ics)
-#     print(",".join(ics))
 
 
